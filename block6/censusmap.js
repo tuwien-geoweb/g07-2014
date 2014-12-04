ac45362b9b1c3bafb93420f72eae371ad95a6642
Clemens Raffler <root676@users.noreply.github.com>
2014-12-04 10:52:22 +0100

    Update censusmap.js

--- block6/censusmap.js
+++ block6/censusmap.js
@@ -5,7 +5,7 @@ var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
 var wmsLayer = new ol.layer.Image({
   source: new ol.source.ImageWMS({
     url: 'http://student.ifip.tuwien.ac.at/geoserver/wms',
-    params: {'LAYERS': 'g07:normalized'}
+    params: {'LAYERS': 'g07_2014:normalized'}
   }),
   opacity: 0.6
 });
