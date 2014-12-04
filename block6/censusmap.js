6efee30fba4fa4c5cd0ea622690ee91525e7ec33
Clemens Raffler <root676@users.noreply.github.com>
2014-12-04 12:56:32 +0100

    Nominatim extension

--- block6/censusmap.js
+++ block6/censusmap.js
@@ -60,3 +60,23 @@ olMap.on('singleclick', function(evt) {
     $('#popup').popover('hide');
   });
 });
+
+// Submit query to Nominatim and zoom map to the result's extent
+var form = document.forms[0];
+form.onsubmit = function(evt) {
+  var url = 'http://nominatim.openstreetmap.org/search?format=json&q=';
+  url += form.query.value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = function() {
+    var result = JSON.parse(xhr.responseText);
+    if (result.length > 0) {
+      var bbox = result[0].boundingbox;
+      olMap.getView().fitExtent(ol.proj.transform([parseFloat(bbox[2]),
+          parseFloat(bbox[0]), parseFloat(bbox[3]), parseFloat(bbox[1])],
+          'EPSG:4326', 'EPSG:3857'), olMap.getSize());
+    }
+  };
+  xhr.send();
+  evt.preventDefault();
+};
