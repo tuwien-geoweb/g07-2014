aefb5fc1bae9a929a853a3e98c2944f0a4e171a8
Clemens Raffler <root676@users.noreply.github.com>
2015-01-09 14:47:43 +0100

    Update wohnstandortanalyse.js

--- wohnstandortanalyse/wohnstandortanalyse.js
+++ wohnstandortanalyse/wohnstandortanalyse.js
@@ -143,7 +143,8 @@
 	  var view = map.getView();
 	  var url = lay_p_wohnstandorte_query_zbez_voronoi.getSource().getGetFeatureInfoUrl(evt.coordinate,
 	      view.getResolution(), view.getProjection(), {'INFO_FORMAT': 'text/html'});
-	  console.log(evt.coordinate);
+	  var coordinate_of_wohnstandort = evt.coordinate;
+	  console.log(coordinate_of_wohnstandort);
 	  popup.setPosition(evt.coordinate);
 	  $('#popup-content iframe').attr('src', url);
 	  $('#popup')
