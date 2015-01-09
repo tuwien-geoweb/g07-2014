f453a6c256c2acd3256f2512cdcf71f11b2d868a
Clemens Raffler <root676@users.noreply.github.com>
2015-01-09 18:34:19 +0100

    Update wohnstandortanalyse.js

--- wohnstandortanalyse/wohnstandortanalyse.js
+++ wohnstandortanalyse/wohnstandortanalyse.js
@@ -231,13 +231,13 @@
 		}),
 		style: new ol.style.Style({
 		     image: new ol.style.Circle({
-		       radius: 20,
+		       radius: 4,
 		       fill: new ol.style.Fill({
 		         color: [251, 187, 16, 1]
 		       }),
 		       stroke: new ol.style.Stroke({
 		         color: [0, 0, 0, 1],
-		         width: 2
+		         width: 0.5
 		       })
 		     })
 		})
