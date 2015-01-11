828fb5033c3354d8949a9017c0cdc5bef9f2e10f
Isaak <isaakgranzer@iMac.local>
2015-01-11 15:27:07 +0100

    Legende: AntiAliasing

--- wohnstandortanalyse/wohnstandortanalyse.js
+++ wohnstandortanalyse/wohnstandortanalyse.js
@@ -294,7 +294,7 @@ $('#topics').change(function() {
        	map.removeLayer(lay_p_wohnstandorte_query_zbez_voronoi);
        	map.addLayer(lay_p_wohnstandorte_query_zbez_voronoi);
        	//insert Legend in map div
-        $('#map').prepend('<img id ="legend" class="legend img-rounded" src="http://student.ifip.tuwien.ac.at/geoserver/g07_2014/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&STRICT=false&style=g07_zbez_dropdown_view" />');
+        $('#map').prepend('<img id ="legend" class="legend img-rounded" src="http://student.ifip.tuwien.ac.at/geoserver/g07_2014/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&STRICT=false&style=g07_zbez_dropdown_view&legend_options=fontAntiAliasing:true" />');
        }
        });
        
